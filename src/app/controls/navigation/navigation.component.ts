c9f224b59d5316ac1ee6371904b5fa4278cae4fa
Gartenschlaeger <mail@kaisnet.de>
2022-03-16 08:35:17 +0100

    change to sidebar nav

--- src/app/controls/navigation/navigation.component.ts
+++ src/app/controls/navigation/navigation.component.ts
@@ -1,11 +1,46 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core'
+
+export interface INavigationItem {
+    title: string
+    routerLink: string
+    isVisible: boolean
+}
 
 @Component({
     selector: 'app-navigation',
     templateUrl: './navigation.component.html'
 })
 export class NavigationComponent implements OnInit {
-    constructor() {}
+    @ViewChild('searchInput') searchInput!: ElementRef<HTMLInputElement>
 
-    ngOnInit(): void {}
+    items: INavigationItem[] = []
+
+    ngOnInit(): void {
+        this.items.push({
+            title: 'Home',
+            routerLink: '/home',
+            isVisible: true
+        })
+        this.items.push({
+            title: 'URL Analyzer',
+            routerLink: '/url-analyzer',
+            isVisible: true
+        })
+        this.items.push({
+            title: 'Docker Run',
+            routerLink: '/docker-run',
+            isVisible: true
+        })
+    }
+
+    handleSearch(e: KeyboardEvent) {
+        if (e.key === 'Escape') {
+            this.searchInput.nativeElement.value = ''
+        }
+
+        const searchValue = this.searchInput.nativeElement.value.toLowerCase()
+        this.items.forEach((item) => {
+            item.isVisible = item.title.toLowerCase().indexOf(searchValue) !== -1
+        })
+    }
 }
