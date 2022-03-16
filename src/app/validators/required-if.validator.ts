0209c1209dd7dd0dac0dae63bd71d2a7b9ec0e64
Gartenschlaeger <mail@kaisnet.de>
2022-03-16 23:23:57 +0100

    code refactoring

--- src/app/validators/required-if.validator.ts
+++ src/app/validators/required-if.validator.ts
@@ -1,20 +1,19 @@
 import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms'
 
-// TODO: make generic
 export function requiredIfValidator(
     mainControl: string,
     mainControlValue: string,
     requiredControl: string
 ): ValidatorFn {
     return (control: AbstractControl): ValidationErrors | null => {
-        const networkMode = control.get(mainControl)?.value
-        const networkName = control.get(requiredControl)?.value
+        const valueMain = control.get(mainControl)?.value
+        const valueRequired = control.get(requiredControl)?.value
 
         if (
-            networkMode &&
-            networkMode === mainControlValue &&
-            typeof networkName === 'string' &&
-            networkName.length === 0
+            valueMain &&
+            valueMain === mainControlValue &&
+            typeof valueRequired === 'string' &&
+            valueRequired.length === 0
         ) {
             return { requiredIf: true }
         }
