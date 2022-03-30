244f204ae8ee8375b79009c46fb021a168bee3b0
Gartenschlaeger <mail@kaisnet.de>
2022-03-30 21:44:11 +0200

    add hslToRgb function

--- src/app/utilities/colorconverter.ts
+++ src/app/utilities/colorconverter.ts
@@ -14,4 +14,15 @@ function rbgToHsl(r: number, g: number, b: number) {
 	]
 }
 
-export { rbgToHsl }
+const hslToRgb = (h: number, s: number, l: number) => {
+	s /= 100
+	l /= 100
+
+	const k = (n: number) => (n + h / 30) % 12
+	const a = s * Math.min(l, 1 - l)
+	const f = (n: number) => l - a * Math.max(-1, Math.min(k(n) - 3, Math.min(9 - k(n), 1)))
+
+	return [255 * f(0), 255 * f(8), 255 * f(4)]
+}
+
+export { rbgToHsl, hslToRgb }
