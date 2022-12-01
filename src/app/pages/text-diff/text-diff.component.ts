6083e0814703970420c54a973ba5d961eadcbc23
Kai Gartenschläger <mail@kaisnet.de>
2022-12-01 21:55:45 +0100

    add compare results

--- src/app/pages/text-diff/text-diff.component.ts
+++ src/app/pages/text-diff/text-diff.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 import { FormService } from '../../modules/form/services/form-service.service'
+import { DiffPart } from './entities/DiffPart'
+import { DiffTableFormat } from './entities/DiffTableFormat'
+import { DiffTableRowResult } from './entities/DiffTableRowResult'
 import { TextDiffService } from './services/text-diff-service'
 
 class TextDiffFormModel {
@@ -12,10 +15,19 @@ const FormDefaults = new TextDiffFormModel()
 
 @Component({
 	selector: 'app-text-diff',
-	templateUrl: './text-diff.component.html'
+	templateUrl: './text-diff.component.html',
+	styleUrls: ['./text-diff.component.css']
 })
 export class TextDiffComponent {
 	form!: FormGroup
+
+	format: DiffTableFormat = 'SideBySide'
+
+	tableRows?: DiffTableRowResult[]
+	tableRowsLineByLine?: DiffTableRowResult[]
+	diffsCount?: number
+	filteredTableRows?: DiffTableRowResult[]
+	filteredTableRowsLineByLine?: DiffTableRowResult[]
 
 	constructor(private fb: FormBuilder, private formService: FormService, private textDiffService: TextDiffService) {
 		this.form = this.defineForm()
@@ -28,20 +40,65 @@ export class TextDiffComponent {
 		})
 	}
 
+	trackTableRows(index: number, row: DiffTableRowResult) {
+		return row && row.leftContent
+			? row.leftContent.lineContent
+			: row && row.rightContent
+			? row.rightContent.lineContent
+			: undefined
+	}
+
+	trackDiffs(index: number, diff: DiffPart) {
+		return diff && diff.content ? diff.content : undefined
+	}
+
 	async handleSubmit() {
 		if (this.formService.validate(this.form)) {
 			const model: TextDiffFormModel = this.form.value
-			const diff = await this.textDiffService.getDiffsByLines(model.left, model.right)
 
-			// TODO: visualize diffs
-			console.log(diff)
+			this.tableRows = await this.textDiffService.getDiffsByLines(model.left, model.right)
+			this.tableRowsLineByLine = this.tableRows.reduce(
+				(tableLineByLine: DiffTableRowResult[], row: DiffTableRowResult) => {
+					if (!tableLineByLine) {
+						tableLineByLine = []
+					}
 
-			// this.result = diff
+					if (row.hasDiffs) {
+						if (row.leftContent) {
+							tableLineByLine.push({
+								leftContent: row.leftContent,
+								rightContent: undefined,
+								belongTo: row.belongTo,
+								hasDiffs: true,
+								numDiffs: row.numDiffs
+							})
+						}
+
+						if (row.rightContent) {
+							tableLineByLine.push({
+								leftContent: undefined,
+								rightContent: row.rightContent,
+								belongTo: row.belongTo,
+								hasDiffs: true,
+								numDiffs: row.numDiffs
+							})
+						}
+					} else {
+						tableLineByLine.push(row)
+					}
+
+					return tableLineByLine
+				},
+				[]
+			)
+
+			this.diffsCount = this.tableRows.filter((row) => row.hasDiffs).length
+			this.filteredTableRows = this.tableRows
+			this.filteredTableRowsLineByLine = this.tableRowsLineByLine
 		}
 	}
 
 	handleReset() {
 		this.formService.reset(this.form, FormDefaults)
-		//this.result = null
 	}
 }
--- src/app/pages/text-diff/text-diff.module.ts
+++ src/app/pages/text-diff/text-diff.module.ts
@@ -2,6 +2,7 @@ import { CommonModule } from '@angular/common'
 import { NgModule } from '@angular/core'
 import { Route, RouterModule } from '@angular/router'
 import { SharedModule } from '../../modules/shared/shared.module'
+import { FormatLinePipe } from './pipes/format-line.pipe'
 import { TextDiffComponent } from './text-diff.component'
 
 const routes: Route[] = [
@@ -12,7 +13,7 @@ const routes: Route[] = [
 ]
 
 @NgModule({
-	declarations: [TextDiffComponent],
+	declarations: [TextDiffComponent, FormatLinePipe],
 	imports: [CommonModule, RouterModule.forChild(routes), SharedModule]
 })
 export class TextDiffModule {}
