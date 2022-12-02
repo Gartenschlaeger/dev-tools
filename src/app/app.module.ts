03207c48d6f172d090203600a4f698fc35b32ceb
Kai Gartenschläger <mail@kaisnet.de>
2022-12-03 00:40:57 +0100

    migrate tools to material design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core'
 import { MatButtonModule } from '@angular/material/button'
 import { MatCardModule } from '@angular/material/card'
+import { MatCheckboxModule } from '@angular/material/checkbox'
 import { MatFormFieldModule } from '@angular/material/form-field'
 import { MatIconModule } from '@angular/material/icon'
 import { MatInputModule } from '@angular/material/input'
@@ -64,6 +65,7 @@ import { UrlAnalyzerPageComponent } from './pages/url-analyzer-page/url-analyzer
 		MatButtonModule,
 		MatSidenavModule,
 		MatFormFieldModule,
+		MatCheckboxModule,
 		MatInputModule,
 		MatSelectModule,
 		MatListModule,
