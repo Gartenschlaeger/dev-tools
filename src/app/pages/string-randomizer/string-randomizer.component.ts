5345fd00d737cfdd45ef057f436bb47c92f2ad05
Kai Gartenschläger <mail@kaisnet.de>
2022-12-03 17:24:34 +0100

    migrate string randomizer to material design

--- src/app/pages/string-randomizer/string-randomizer.component.ts
+++ src/app/pages/string-randomizer/string-randomizer.component.ts
@@ -1,71 +1,72 @@
-import { Component, OnInit } from '@angular/core'
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms'
-import { FormService } from '../../modules/form/services/form-service.service'
-import { PageService } from '../../utilities/page-service'
-import * as randomizer from '../../utilities/randomizer'
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { FormService } from '../../modules/form/services/form-service.service';
+import { PageService } from '../../utilities/page-service';
+import * as randomizer from '../../utilities/randomizer';
 
 export class StringRandomizerFormModel {
-	sourceText: string = ''
-	trimResult: boolean = false
-	splitIntoWords: boolean = false
+    sourceText: string = '';
+    trimResult: boolean = false;
+    splitIntoWords: boolean = false;
 }
 
 export class StringRandomizerResultModel {
-	randomizedValue?: string
+    randomizedValue?: string;
 }
 
-const FormDefaults = new StringRandomizerFormModel()
+const FormDefaults = new StringRandomizerFormModel();
 
 @Component({
-	selector: 'app-string-randomizer',
-	templateUrl: './string-randomizer.component.html'
+    selector: 'app-string-randomizer',
+    templateUrl: './string-randomizer.component.html'
 })
 export class StringRandomizerComponent implements OnInit {
-	form!: UntypedFormGroup
-	result?: StringRandomizerResultModel
+    form!: UntypedFormGroup;
+    result?: StringRandomizerResultModel;
 
-	constructor(private fb: UntypedFormBuilder, private formService: FormService, private pageService: PageService) {}
+    constructor(private fb: UntypedFormBuilder, private formService: FormService, private pageService: PageService) {
+    }
 
-	ngOnInit() {
-		this.form = this.defineForm()
+    ngOnInit() {
+        this.form = this.defineForm();
 
-		this.pageService.setPageTitle('String Randomizer')
-	}
+        this.pageService.setPageTitle('String Randomizer');
+    }
 
-	defineForm(): UntypedFormGroup {
-		return this.fb.group({
-			sourceText: [FormDefaults.sourceText, [Validators.required]],
-			trimResult: [FormDefaults.trimResult],
-			splitIntoWords: [FormDefaults.splitIntoWords]
-		})
-	}
+    defineForm(): UntypedFormGroup {
+        return this.fb.group({
+            sourceText: [FormDefaults.sourceText, [Validators.required]],
+            trimResult: [FormDefaults.trimResult],
+            splitIntoWords: [FormDefaults.splitIntoWords]
+        });
+    }
 
-	handleReset() {
-		this.formService.reset(this.form, FormDefaults)
-		this.result = undefined
-	}
+    handleReset() {
+        this.formService.reset(this.form, FormDefaults);
+        this.result = undefined;
+    }
 
-	handleSubmit() {
-		if (this.formService.validate(this.form)) {
-			const model: StringRandomizerFormModel = this.form.value
+    handleSubmit() {
+        if (this.formService.validate(this.form)) {
+            const model: StringRandomizerFormModel = this.form.value;
 
-			let randomizedValue: string
+            let randomizedValue: string;
 
-			if (model.splitIntoWords) {
-				const words = model.sourceText.split(' ')
-				const shuffledArray = randomizer.shuffleArray(words)
-				randomizedValue = shuffledArray.join(' ')
-			} else {
-				randomizedValue = randomizer.shuffleString(model.sourceText)
-			}
+            if (model.splitIntoWords) {
+                const words = model.sourceText.split(' ');
+                const shuffledArray = randomizer.shuffleArray(words);
+                randomizedValue = shuffledArray.join(' ');
+            } else {
+                randomizedValue = randomizer.shuffleString(model.sourceText);
+            }
 
-			if (model.trimResult) {
-				randomizedValue = randomizedValue.trim()
-			}
+            if (model.trimResult) {
+                randomizedValue = randomizedValue.trim();
+            }
 
-			this.result = {
-				randomizedValue
-			}
-		}
-	}
+            this.result = {
+                randomizedValue
+            };
+        }
+    }
 }
--- src/app/pages/url-encoder/url-encoder.component.ts
+++ src/app/pages/url-encoder/url-encoder.component.ts
@@ -1,90 +1,90 @@
-import { Component, OnInit } from '@angular/core'
-import { UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms'
-import { ActivatedRoute } from '@angular/router'
-import { FormService } from 'src/app/modules/form/services/form-service.service'
-import { PageService } from '../../utilities/page-service'
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormControl, UntypedFormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { FormService } from 'src/app/modules/form/services/form-service.service';
+import { PageService } from '../../utilities/page-service';
 
 export class UrlEncoderFormModel {
-	sourceValue: string = ''
-	encodeAsQueryString: boolean = false
+    sourceValue: string = '';
+    encodeAsQueryString: boolean = false;
 }
 
 export interface UrlEncoderResultModel {
-	processedValue: string
-	hasErrors: boolean
+    processedValue: string;
+    hasErrors: boolean;
 }
 
-const FormDefaults = new UrlEncoderFormModel()
+const FormDefaults = new UrlEncoderFormModel();
 
 @Component({
-	selector: 'app-url-encoder',
-	templateUrl: './url-encoder.component.html'
+    selector: 'app-url-encoder',
+    templateUrl: './url-encoder.component.html'
 })
 export class URLEncoderComponent implements OnInit {
-	form!: UntypedFormGroup
-	isEncodeMode!: boolean
-	pageTitle!: string
-	result?: UrlEncoderResultModel
+    form!: UntypedFormGroup;
+    isEncodeMode!: boolean;
+    pageTitle!: string;
+    result?: UrlEncoderResultModel;
 
-	constructor(
-		route: ActivatedRoute,
-		private fb: UntypedFormBuilder,
-		private formService: FormService,
-		private pageService: PageService
-	) {
-		const isEncodeMode = route.snapshot.url[0].path === 'url-encoder'
+    constructor(
+        route: ActivatedRoute,
+        private fb: UntypedFormBuilder,
+        private formService: FormService,
+        private pageService: PageService
+    ) {
+        const isEncodeMode = route.snapshot.url[0].path === 'url-encoder';
 
-		this.isEncodeMode = isEncodeMode
-		this.pageTitle = isEncodeMode ? 'URL Encoder' : 'URL Decoder'
-	}
+        this.isEncodeMode = isEncodeMode;
+        this.pageTitle = isEncodeMode ? 'URL Encoder' : 'URL Decoder';
+    }
 
-	ngOnInit() {
-		this.form = this.defineForm()
+    ngOnInit() {
+        this.form = this.defineForm();
 
-		this.pageService.setPageTitle(this.pageTitle)
-	}
+        this.pageService.setPageTitle(this.pageTitle);
+    }
 
-	defineForm(): UntypedFormGroup {
-		const result = this.fb.group({
-			sourceValue: [FormDefaults.sourceValue, [Validators.required]]
-		})
+    defineForm(): UntypedFormGroup {
+        const result = this.fb.group({
+            sourceValue: [FormDefaults.sourceValue, [Validators.required]]
+        });
 
-		if (this.isEncodeMode) {
-			result.addControl('encodeAsQueryString', new UntypedFormControl(false))
-		}
+        if (this.isEncodeMode) {
+            result.addControl('encodeAsQueryString', new UntypedFormControl(false));
+        }
 
-		return result
-	}
+        return result;
+    }
 
-	handleSubmit() {
-		if (this.formService.validate(this.form)) {
-			const model: UrlEncoderFormModel = this.form.value
+    handleSubmit() {
+        if (this.formService.validate(this.form)) {
+            const model: UrlEncoderFormModel = this.form.value;
 
-			try {
-				let processedValue: string
-				if (this.isEncodeMode) {
-					processedValue = model.encodeAsQueryString
-						? encodeURIComponent(model.sourceValue)
-						: encodeURI(model.sourceValue)
-				} else {
-					processedValue = decodeURIComponent(model.sourceValue)
-				}
+            try {
+                let processedValue: string;
+                if (this.isEncodeMode) {
+                    processedValue = model.encodeAsQueryString
+                        ? encodeURIComponent(model.sourceValue)
+                        : encodeURI(model.sourceValue);
+                } else {
+                    processedValue = decodeURIComponent(model.sourceValue);
+                }
 
-				this.result = {
-					processedValue,
-					hasErrors: false
-				}
-			} catch (error) {
-				this.result = {
-					processedValue: '',
-					hasErrors: true
-				}
-			}
-		}
-	}
+                this.result = {
+                    processedValue,
+                    hasErrors: false
+                };
+            } catch (error) {
+                this.result = {
+                    processedValue: '',
+                    hasErrors: true
+                };
+            }
+        }
+    }
 
-	handleReset() {
-		this.result = undefined
-		this.formService.reset(this.form, FormDefaults)
-	}
+    handleReset() {
+        this.result = undefined;
+        this.formService.reset(this.form, FormDefaults);
+    }
 }
