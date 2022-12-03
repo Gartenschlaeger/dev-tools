3e6c8da02a913db208257d753c6219c4455bd47c
Kai Gartenschläger <mail@kaisnet.de>
2022-12-03 17:52:35 +0100

    migrate json path to material design

--- src/app/pages/json-path/json-path.component.ts
+++ src/app/pages/json-path/json-path.component.ts
@@ -1,72 +1,73 @@
-import { Component, OnInit } from '@angular/core'
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms'
-import * as jsonpath from 'jsonpath'
-import { FormService } from '../../modules/form/services/form-service.service'
-import { PageService } from '../../utilities/page-service'
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import * as jsonpath from 'jsonpath';
+import { FormService } from '../../modules/form/services/form-service.service';
+import { PageService } from '../../utilities/page-service';
 
 class JsonPathFormModule {
-	json: string = ''
-	path: string = ''
+    json: string = '';
+    path: string = '';
 }
 
 class JsonPathResult {
-	result?: string
-	error?: string
+    result?: string;
+    error?: string;
 }
 
-const FormDefaults = new JsonPathFormModule()
+const FormDefaults = new JsonPathFormModule();
 
 @Component({
-	selector: 'app-json-path',
-	templateUrl: './json-path.component.html'
+    selector: 'app-json-path',
+    templateUrl: './json-path.component.html'
 })
 export class JsonPathComponent implements OnInit {
-	form!: UntypedFormGroup
-	result: JsonPathResult | null = null
+    form!: UntypedFormGroup;
+    result: JsonPathResult | null = null;
 
-	constructor(private fb: UntypedFormBuilder, private formService: FormService, private pageService: PageService) {}
+    constructor(private fb: UntypedFormBuilder, private formService: FormService, private pageService: PageService) {
+    }
 
-	ngOnInit() {
-		this.form = this.defineForm()
+    ngOnInit() {
+        this.form = this.defineForm();
 
-		this.pageService.setPageTitle('JSON Path')
-	}
+        this.pageService.setPageTitle('JSON Path');
+    }
 
-	defineForm(): UntypedFormGroup {
-		return this.fb.group({
-			json: [FormDefaults.json, [Validators.required]],
-			path: [FormDefaults.path, [Validators.required]]
-		})
-	}
+    defineForm(): UntypedFormGroup {
+        return this.fb.group({
+            json: [FormDefaults.json, [Validators.required]],
+            path: [FormDefaults.path, [Validators.required]]
+        });
+    }
 
-	handleReset() {
-		this.formService.reset(this.form, FormDefaults)
-		this.result = null
-	}
+    handleReset() {
+        this.formService.reset(this.form, FormDefaults);
+        this.result = null;
+    }
 
-	handleSubmit() {
-		if (this.formService.validate(this.form)) {
-			const model: JsonPathFormModule = this.form.value
+    handleSubmit() {
+        if (this.formService.validate(this.form)) {
+            const model: JsonPathFormModule = this.form.value;
 
-			let tmp = new JsonPathResult()
+            let tmp = new JsonPathResult();
 
-			let inputObj: any
-			try {
-				inputObj = JSON.parse(model.json!)
-			} catch (err: any) {
-				tmp.error = 'Error while parsing the input JSON : "\n' + err?.message + '"'
-			}
+            let inputObj: any;
+            try {
+                inputObj = JSON.parse(model.json!);
+            } catch (err: any) {
+                tmp.error = 'Error while parsing the input JSON : "\n' + err?.message + '"';
+            }
 
-			let pathResult: any
-			try {
-				pathResult = jsonpath.query(inputObj, model.path!)
-			} catch (err: any) {
-				tmp.error = 'Error while evaluating the path query : "\n' + err.message + '"'
-			}
+            let pathResult: any;
+            try {
+                pathResult = jsonpath.query(inputObj, model.path!);
+            } catch (err: any) {
+                tmp.error = 'Error while evaluating the path query : "\n' + err.message + '"';
+            }
 
-			tmp.result = JSON.stringify(pathResult, null, '  ')
+            tmp.result = JSON.stringify(pathResult, null, '  ');
 
-			this.result = tmp
-		}
-	}
+            this.result = tmp;
+        }
+    }
 }
