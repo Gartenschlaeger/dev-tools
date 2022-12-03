4ddae675517fa8523c3e635e346475a8ee7c08ee
Kai Gartenschläger <mail@kaisnet.de>
2022-12-03 17:42:35 +0100

    migrate json parser to material design

--- src/app/pages/json-parser/json-parser.component.ts
+++ src/app/pages/json-parser/json-parser.component.ts
@@ -1,82 +1,83 @@
-import { Component, OnInit } from '@angular/core'
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms'
-import { NavigationExtras, Router } from '@angular/router'
-import { FormService } from '../../modules/form/services/form-service.service'
+import { Component, OnInit } from '@angular/core';
+import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import { NavigationExtras, Router } from '@angular/router';
+import { FormService } from '../../modules/form/services/form-service.service';
 
 export class JsonParserFormModel {
-	source: string = ''
+    source: string = '';
 }
 
 export class JsonParserResultModel {
-	formattedValue?: string
-	errorMessage?: string
+    formattedValue?: string;
+    errorMessage?: string;
 
-	reset() {
-		this.formattedValue = undefined
-		this.errorMessage = undefined
-	}
+    reset() {
+        this.formattedValue = undefined;
+        this.errorMessage = undefined;
+    }
 }
 
-const FormDefaults = new JsonParserFormModel()
+const FormDefaults = new JsonParserFormModel();
 
 @Component({
-	selector: 'app-json-parser',
-	templateUrl: './json-parser.component.html'
+    selector: 'app-json-parser',
+    templateUrl: './json-parser.component.html'
 })
 export class JsonParserComponent implements OnInit {
-	form!: UntypedFormGroup
-	result!: JsonParserResultModel
+    form!: UntypedFormGroup;
+    result!: JsonParserResultModel;
 
-	constructor(private fb: UntypedFormBuilder, private formService: FormService, private router: Router) {}
+    constructor(private fb: UntypedFormBuilder, private formService: FormService, private router: Router) {
+    }
 
-	ngOnInit(): void {
-		this.form = this.defineForm()
-		this.result = new JsonParserResultModel()
-	}
+    ngOnInit(): void {
+        this.form = this.defineForm();
+        this.result = new JsonParserResultModel();
+    }
 
-	defineForm(): UntypedFormGroup {
-		return this.fb.group({
-			source: [FormDefaults.source, [Validators.required]]
-		})
-	}
+    defineForm(): UntypedFormGroup {
+        return this.fb.group({
+            source: [FormDefaults.source, [Validators.required]]
+        });
+    }
 
-	handleReset() {
-		this.formService.reset(this.form)
-		this.result.reset()
-	}
+    handleReset() {
+        this.formService.reset(this.form);
+        this.result.reset();
+    }
 
-	handlePassToJsonFormatter() {
-		if (this.result.formattedValue === undefined) {
-			return
-		}
+    handlePassToJsonFormatter() {
+        if (this.result.formattedValue === undefined) {
+            return;
+        }
 
-		const extra: NavigationExtras = {
-			state: {
-				source: this.result.formattedValue
-			}
-		}
+        const extra: NavigationExtras = {
+            state: {
+                source: this.result.formattedValue
+            }
+        };
 
-		this.router.navigate(['/json-formatter'], extra)
-	}
+        this.router.navigate(['/json-formatter'], extra);
+    }
 
-	handleSubmit() {
-		if (this.formService.validate(this.form)) {
-			const model: JsonParserFormModel = this.form.value
-			this.result.reset()
+    handleSubmit() {
+        if (this.formService.validate(this.form)) {
+            const model: JsonParserFormModel = this.form.value;
+            this.result.reset();
 
-			try {
-				let parsedObj = JSON.parse(model.source)
-				while (typeof parsedObj !== 'object') {
-					parsedObj = JSON.parse(parsedObj)
-				}
+            try {
+                let parsedObj = JSON.parse(model.source);
+                while (typeof parsedObj !== 'object') {
+                    parsedObj = JSON.parse(parsedObj);
+                }
 
-				this.result.formattedValue = JSON.stringify(parsedObj, null, '  ')
-			} catch (err: unknown) {
-				this.result.errorMessage = 'Failed to parse json'
-				if (err instanceof Error) {
-					this.result.errorMessage += ': "' + err.message + '"'
-				}
-			}
-		}
-	}
+                this.result.formattedValue = JSON.stringify(parsedObj, null, '  ');
+            } catch (err: unknown) {
+                this.result.errorMessage = 'Failed to parse json';
+                if (err instanceof Error) {
+                    this.result.errorMessage += ': "' + err.message + '"';
+                }
+            }
+        }
+    }
 }
--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core'
-import { PageService } from '../../utilities/page-service'
+import { Component, OnInit } from '@angular/core';
+import { PageService } from '../../utilities/page-service';
 
 @Component({
-	selector: 'app-not-found',
-	templateUrl: './not-found.component.html'
+    selector: 'app-not-found',
+    templateUrl: './not-found.component.html'
 })
 export class NotFoundComponent implements OnInit {
-	constructor(private pageService: PageService) {}
+    constructor(private pageService: PageService) {
+    }
 
-	ngOnInit() {
-		this.pageService.resetTitle()
-	}
+    ngOnInit() {
+        this.pageService.resetTitle();
+    }
 }
