eefb77c0437dc51200b4fdb35d3b6ab38d06cae9
Kai Gartenschläger <mail@kaisnet.de>
2022-12-20 11:41:25 +0100

    adjust imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
-import { NavigationComponent } from 'src/app/components/navigation/navigation.component';
-import { SharedModule } from 'src/app/modules/shared/shared.module';
 import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 import { routes } from './app.routes';
+import { NavigationComponent } from './components/navigation/navigation.component';
 import { TextInputDialogComponent } from './components/shared-dialogs/dialogs/text-input-dialog.component';
+import { SharedModule } from './modules/shared/shared.module';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
