bb0620cc787baeca43c06ea7bb4e67d845abb2a3
Kai Gartenschläger <mail@kaisnet.de>
2024-05-09 11:05:31 +0200

    show objects correcly

--- src/app/pages/json-formatter/services/json-flat-list-formatter.service.ts
+++ src/app/pages/json-formatter/services/json-flat-list-formatter.service.ts
@@ -11,10 +11,16 @@ export class JsonFlatListParserService {
 
                 if (Array.isArray(json[key])) {
                     json[key].forEach((element: any, index: number) => {
-                        result.push(`${fullPath}[${index}] = ${JSON.stringify(element)}`);
+                        if (typeof element === 'object' && element !== null) {
+                            const subResult = this.convertToJsonPathValue(element, `${fullPath}[${index}]`);
+                            result = result.concat(subResult);
+                        } else {
+                            result.push(`${fullPath}[${index}] = ${JSON.stringify(element)}`);
+                        }
                     });
                 } else if (typeof json[key] === 'object' && json[key] !== null) {
-                    result = result.concat(this.convertToJsonPathValue(json[key], fullPath));
+                    const subResult = this.convertToJsonPathValue(json[key], fullPath);
+                    result = result.concat(subResult);
                 } else {
                     result.push(`${fullPath} = ${JSON.stringify(json[key])}`);
                 }
